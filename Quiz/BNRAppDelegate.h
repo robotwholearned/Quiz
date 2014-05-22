d9cd01583a29fd4eaf820e0f6d1359eceaf019fd
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-05-21 20:18:40 -0600

    Initial Commit

---
+++ Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Sandquist, Cassandra G on 5/21/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Sandquist, Cassandra G on 5/21/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+  }
+}
