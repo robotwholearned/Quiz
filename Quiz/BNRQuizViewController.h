837385d7c452a79568f8dee2d7d1124b88ece557
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-05-22 08:18:27 -0600

    Add quiz view controller

---
+++ Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Sandquist, Cassandra G on 5/22/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ Quiz/BNRQuizViewController.m
@@ -0,0 +1,38 @@
+//
+//  BNRQuizViewController.m
+//  Quiz
+//
+//  Created by Sandquist, Cassandra G on 5/22/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import "BNRQuizViewController.h"
+
+@interface BNRQuizViewController ()
+
+@end
+
+@implementation BNRQuizViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
